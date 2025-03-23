97193405b3b06e64ced57176bde8d7d7ff756fc6
Charlen R <charlenr@ghotmail.com>
2025-03-22 22:19:38 -0300

    add aleatory key pix

--- weddingsite/src/components/pages/end/end.tsx
+++ weddingsite/src/components/pages/end/end.tsx
@@ -34,7 +34,7 @@ export default function P5() {
     return <>
         <ScreenBlock className='p5 flex flex-col justify-center items-center w-full h-full'>
             <SepiaMask className='flex justify-center items-center flex-col'>
-                <div className='flex flex-1 lg:flex-none'></div>
+                <div className='flex h-[40%]'></div>
                 <div className="w-full flex sm:max-w-[500px] text-center justify-center items-center cursor-pointer link" onClick={gotoGiftList}>
                     <BigText className="text-2xl"> Lista de presentes </BigText>
                 </div>
@@ -43,7 +43,7 @@ export default function P5() {
                 </div>
                 <div className='text-center flex flex-col h-[20%] w-full max-w-[500px]'>
                     <LittleWhiteText className='text-2xl'>Para nossos amigos que querem nos presentear com Pix, <br />agradecemos com muito carinho também viu?</LittleWhiteText>
-                    <LittleWhiteText className='text-2xl'> Chave Aleatória: ( Clique na chave para copiar )</LittleWhiteText>
+                    <LittleWhiteText className='text-2xl'> Chave Aleatória: <br/>( Clique na chave para copiar )</LittleWhiteText>
                     <div className='w-full lg:text-4xl text-3xl mt-20 cursor-pointer link flex justify-center items-center text-center' onClick={copiarLink}> Copiar Chave PIX </div>
                     <LittleWhiteText id={"text-copied"}></LittleWhiteText>
                 </div>
